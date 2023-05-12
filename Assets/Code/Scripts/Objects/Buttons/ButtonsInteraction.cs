77a2ae3bef7ec92238b7ddb1849d2a762e436c20
Supykes <supykess@gmail.com>
2023-05-12 03:17:26 +0300

    add keypad interaction

--- Assets/Code/Scripts/Objects/Buttons/ButtonsInteraction.cs
+++ Assets/Code/Scripts/Objects/Buttons/ButtonsInteraction.cs
@@ -7,23 +7,40 @@ public class ButtonsInteraction : MonoBehaviour
     public enum ButtonType
     {
         CombinationLockNumber,
-        CombinationLockSubmit
+        CombinationLockSubmit,
+        KeypadNumber,
+        KeypadClear,
+        KeypadSubmit
     }
 
     public ButtonType buttonType;
+    public int keypadNumber;
     public int combinationLockButtonPosition;
+    public TMP_Text keypadNumbersText;
     public TMP_Text combinationLockNumberText;
+    public GameObject keypadDoor;
     public GameObject combinationLockDoor;
+    public GameObject keypad;
     public GameObject combinationLock;
+    CabinetAnimation keypadDoorAnimation;
     CabinetAnimation combinationLockDoorAnimation;
+    string keypadCorrectCode = "1234";
     string combinationLockCorrectCode = "1234";
+    static string keypadCurrentCode = "";
     static string combinationLockCurrentCode = "0000";
     char[] combinationLockCurrentCodeSymbols;
     int combinationLockNumber = 0;
 
     void Start()
     {
-        combinationLockDoorAnimation = combinationLockDoor.GetComponent<CabinetAnimation>();
+        if (buttonType == ButtonType.KeypadNumber || buttonType == ButtonType.KeypadClear || buttonType == ButtonType.KeypadSubmit)
+        {
+            keypadDoorAnimation = keypadDoor.GetComponent<CabinetAnimation>();
+        }
+        else if (buttonType == ButtonType.CombinationLockNumber || buttonType == ButtonType.CombinationLockSubmit)
+        {
+            combinationLockDoorAnimation = combinationLockDoor.GetComponent<CabinetAnimation>();
+        }
     }
 
     public void HandleButtonBehaviour()
@@ -48,6 +65,36 @@ public class ButtonsInteraction : MonoBehaviour
                 combinationLockDoorAnimation.PlayAnimation();
 
                 DisableButtonsInteraction(combinationLock);
+            }
+        }
+        else if (buttonType == ButtonType.KeypadNumber)
+        {
+            if (keypadCurrentCode.Length < 4)
+            {
+                keypadCurrentCode += keypadNumber;
+
+                keypadNumbersText.text = keypadCurrentCode;
+            }
+        }
+        else if (buttonType == ButtonType.KeypadClear)
+        {
+            keypadCurrentCode = "";
+
+            keypadNumbersText.text = keypadCurrentCode;
+        }
+        else if (buttonType == ButtonType.KeypadSubmit)
+        {
+            if (keypadCurrentCode == keypadCorrectCode)
+            {
+                keypadDoorAnimation.PlayAnimation();
+
+                DisableButtonsInteraction(keypad);
+            }
+            else
+            {
+                keypadCurrentCode = "";
+
+                keypadNumbersText.text = keypadCurrentCode;
             }
         }
     }
